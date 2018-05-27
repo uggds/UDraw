055f683f5b05aa990d848157d04c846688576c24
UGA <s.ugagod@gmail.com>
2018-05-27 20:07:36 +0900

    Initial Commit

---
+++ UDraw/UDraw.h
@@ -0,0 +1,19 @@
+//
+//  UDraw.h
+//  UDraw
+//
+//  Created by Seiya Ugajin on 2018/05/27.
+//  Copyright © 2018年 Seiya Ugajin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UDraw.
+FOUNDATION_EXPORT double UDrawVersionNumber;
+
+//! Project version string for UDraw.
+FOUNDATION_EXPORT const unsigned char UDrawVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UDraw/PublicHeader.h>
+
+
